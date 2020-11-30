eb1a2066c772f572a46223f68e815a15171232ae
Paulo Victor <paulovictor@live.fr>
2020-11-30 20:13:03 -0300

    template finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,3 +33,34 @@ function linkAction(){
 
 // add event listener for the link action on each link
 navLink.forEach(n => n.addEventListener('click', linkAction));
+
+// Scrol Reveal Animation
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+});
+
+// Scroll Home
+sr.reveal('.home__title', {});
+sr.reveal('.button', {delay: 200});
+sr.reveal('.home__img', {delay: 400});
+sr.reveal('.home__social-icon', {interval: 200});
+
+// Scroll About
+sr.reveal('.about__title', {});
+sr.reveal('.about__subtitle', {delay: 200}); 
+sr.reveal('.about__text', {delay: 400}); 
+
+// Scroll Skills
+sr.reveal('.skills__subtitle', {});
+sr.reveal('.skills__text', {delay: 200}); 
+sr.reveal('.skills__data', {interval: 200}); 
+sr.reveal('.skills__img', {delay: 200});
+
+// Scroll Work
+sr.reveal('.work__img', {interval: 200});
+
+// Scroll Contact
+sr.reveal('.contact__input',  {interval: 200});
